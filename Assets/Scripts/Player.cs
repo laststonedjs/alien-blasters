ea6ee7be5621727be13492689584863f6b8a551e
lastoned <filipsarovic428@gmail.com>
2026-06-09 21:10:57 +0200

    added gizmos for left and right foot grounding

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,9 +9,10 @@ public class Player : MonoBehaviour
     [SerializeField] float _jumpDuration = 0.5f;
     [SerializeField] Sprite _jumpSprite;
     [SerializeField] LayerMask _layerMask;
+    [SerializeField] float _footOffset = 0.5f;
+
     public bool IsGrounded;
     SpriteRenderer _spriteRenderer;
-    Sprite _defaultSprite;
     float _horizontal;
     Animator _animator;
 
@@ -19,14 +20,22 @@ public class Player : MonoBehaviour
     {
         _animator = GetComponent<Animator>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        _defaultSprite = _spriteRenderer.sprite;
     }
 
     void OnDrawGizmos()
     {
         SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-        Vector2 origin = new Vector2(transform.position.x, transform.position.y - spriteRenderer.bounds.extents.y);
         Gizmos.color = Color.red;
+        
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - spriteRenderer.bounds.extents.y);
+        Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
+        
+        // draw left foot
+        origin = new Vector2(transform.position.x - _footOffset, transform.position.y - spriteRenderer.bounds.extents.y);
+        Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
+
+        // draw right foot
+        origin = new Vector2(transform.position.x + _footOffset, transform.position.y - spriteRenderer.bounds.extents.y);
         Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
     }
 
